a6c5948f20e5305b490060c07ebb7aea715b951e
Hoang Nguyen <vuhoangnguyenga@gmail.com>
2026-06-29 17:48:18 -0700

    add Elevations

---
+++ lib-design-system/src/main/java/app/honguyen/forge/designsystem/theme/Elevations.kt
@@ -0,0 +1,12 @@
+package app.honguyen.forge.designsystem.theme
+
+import androidx.compose.ui.unit.dp
+
+object Elevations {
+    val Level0 = 0.dp
+    val Level1 = 1.dp
+    val Level2 = 3.dp
+    val Level3 = 6.dp
+    val Level4 = 8.dp
+    val Level5 = 12.dp
+}
