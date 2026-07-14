c4baeb53e84a622b135c48579bd1e30e559e8720
Hoang Nguyen <vuhoangnguyenga@gmail.com>
2026-07-13 19:58:56 -0700

    fix lint

--- lib-design-system/src/main/java/app/honguyen/forge/designsystem/uikit/icons/RoundIcon.kt
+++ lib-design-system/src/main/java/app/honguyen/forge/designsystem/uikit/icons/RoundIcon.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
-import app.honguyen.forge.designsystem.modifier.thenIf
 import app.honguyen.forge.designsystem.modifier.thenIfNotNull
 import app.honguyen.forge.designsystem.theme.ForgeTheme
 import app.honguyen.forge.designsystem.theme.icons.FramePerson
